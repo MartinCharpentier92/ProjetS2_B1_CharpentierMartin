205e8d683fab5e0e16386c1b547450b61b2d61a8
Martin Charpentier <charpmartin@gmail.com>
2023-05-16 17:37:11 +0200

    added login.php and login page style

--- _header.php
+++ _header.php
@@ -7,6 +7,10 @@
     <title>Document</title>
     <link rel="stylesheet" href="styles/main.css" />
     <link rel="stylesheet" href="styles/nav.css" />
+    <link rel="stylesheet" href="styles/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 </head>
 <body>
     <?php require_once('_nav.php'); ?>
--- login.php
+++ login.php
@@ -24,30 +24,36 @@
     }
 ?>
 <?php require_once('_header.php'); ?>
-    <form action="" method="post">
+    <form class="login-form" action="" method="post">
+    <div class="login-form-element1">
         <h1>Connexion</h1>
-
+    </div>    
+    
         <?php if (isset($msg)) { echo "<div>" . $msg . "</div>"; } ?>
 
-        <div>
-            <label for="email">Email</label>
-            <input 
-                type="email" 
-                placeholder="Entrez votre email" 
-                name="email" 
-                id="email" 
-            />
-        </div>
-        <div>
-            <label for="password">Mot de passe</label>
-            <input 
-                type="password" 
-                placeholder="Entrez votre mot de passe" 
-                name="password" 
-                id="password" 
-            />
-        </div>
-        <div>
+            <div class="login-form-element2">
+                <label for="email">Email :</label>
+
+                <input 
+                    type="email" 
+                    placeholder="Entrez votre email" 
+                    name="email" 
+                    id="email" 
+                />
+            </div>
+                
+            <div class="login-form-element3">
+                <label for="password">Mot de passe :</label>
+
+                <input 
+                    type="password" 
+                    placeholder="Entrez votre mot de passe" 
+                    name="password" 
+                    id="password" 
+                />
+            </div>
+            
+        <div class="login-form-element4">
             <input type="submit" name="send" value="Connexion" />
         </div>
     </form>
